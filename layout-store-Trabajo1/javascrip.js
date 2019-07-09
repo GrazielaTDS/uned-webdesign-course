101d781964105932d71bd3933c7663093cf42ccc
GrazielaTDS <graziela.projects@gmail.com>
2019-07-09 20:17:18 +0200

    Slide Articles finished

--- layout-store-Trabajo1/javascrip.js
+++ layout-store-Trabajo1/javascrip.js
@@ -1,6 +1,10 @@
+var slideIndex = 1;
+
 function iniciar() {
-    var slideIndex = 0;
-    showCarrousel(slideIndex);
+    var carrouselIndex = 0;
+    showCarrousel(carrouselIndex);
+
+    showSlides();
 }
 
 function showCarrousel(initialIndex) {
@@ -20,4 +24,24 @@ function showCarrousel(initialIndex) {
     dots[initialIndex - 1].className += " active";
     setTimeout(showCarrousel,4000, initialIndex);
 }
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlide-article");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  slides[slideIndex-1].style.display = "block";  
+}
+
 window.addEventListener('load', iniciar, false);
